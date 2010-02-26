b87d0e458ff89fd65f12211644653e5c5dc5b34c
Marcus Westin <marcus.westin@gmail.com>
2010-02-25 16:08:14 -0800

    Get an XML parser going. Make it work in node as well as in the browser

---
+++ run_xml_with_node.js
@@ -0,0 +1,24 @@
+require('./lib/ometa-js/ometa-node');
+var fs = require('fs')
+var sys = require('sys')
+
+var ometaCode = fs.readFileSync('./parsers/xml.ometa') + "\n"
+try {
+	ometa(ometaCode)
+	
+	var calcSource = fs.readFileSync('./sample_input/test2.xml');
+	
+	var tree = XMLParser.matchAll(calcSource, "xml");
+	sys.puts(tree)
+	
+} catch(e) {
+	error = true
+	var errorMsg = "Error: " + e;
+	if (e.errorPos) { 
+		errorMsg += " at pos " + e.errorPos + " >>>" + ometaCode.substr(e.errorPos, 30) 
+	}
+	sys.puts(errorMsg)
+}
+
+// var output = CalcCompiler.match(tree, "xml");
+// fs.writeFileSync("./output.js", output + "\n");
