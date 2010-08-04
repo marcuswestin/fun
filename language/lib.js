680461c2fec1b367dce51aca8260b080051b8239
Marcus Westin <marcus.westin@gmail.com>
2010-08-03 21:45:42 -0400

    Put the lib.js code in a closure

--- language/lib.js
+++ language/lib.js
@@ -1,19 +1,23 @@
-var fun = {},
-	doc = document
-
-fun.getDomHook = function() {
-	return doc.body.appendChild(doc.createElement('div'))
-}
-
-fun.on = function(element, eventName, handler) {
-	if (element.addEventListener) {
-		element.addEventListener(eventName, handler, false)
-	} else if (element.attachEvent){
-		element.attachEvent("on"+eventName, handler)
+(function() {
+	jsio('from shared.javascript import blockCallback')
+	
+	var fun = window.fun = {},
+		doc = document
+	
+	fun.getDomHook = function() {
+		return doc.body.appendChild(doc.createElement('div'))
 	}
-}
 
-fun.on(document, 'mousemove', function(e) {
-	fin.setLocal('mouseX', e.clientX)
-	fin.setLocal('mouseY', e.clientY)
-})
+	fun.on = function(element, eventName, handler) {
+		if (element.addEventListener) {
+			element.addEventListener(eventName, handler, false)
+		} else if (element.attachEvent){
+			element.attachEvent("on"+eventName, handler)
+		}
+	}
+	
+	fun.on(document, 'mousemove', function(e) {
+		fin.setLocal('mouseX', e.clientX)
+		fin.setLocal('mouseY', e.clientY)
+	})
+})();
