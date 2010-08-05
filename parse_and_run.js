92243c70aeeec8b4b26123db5b6fd7d776360568
Marcus Westin <marcus.westin@gmail.com>
2010-08-05 13:13:24 -0700

    Run fun_code/if_else.fun by default rather than mouseXY.fun

--- parse_and_run.js
+++ parse_and_run.js
@@ -10,7 +10,7 @@ var sys = require('sys'),
  * Parse commandline options *
  *****************************/
 var args = process.argv.slice(2),
-	opts = { code: 'mouseXY.fun', verbose: "true" }
+	opts = { code: 'fun_code/if_else.fun', verbose: "true" }
 
 void(function(){
 	for (var i=0, arg; arg = args[i]; i++) {
