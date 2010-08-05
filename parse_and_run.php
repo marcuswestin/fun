b67efe3100f13926f44747e6b89f1ff032343a3e
Marcus Westin <marcus.westin@gmail.com>
2010-08-05 13:13:38 -0700

    Clean up parse_and_run.php

--- parse_and_run.php
+++ parse_and_run.php
@@ -3,8 +3,8 @@
     $code = isset($_GET['code']) ? "code=fun_code/" . $_GET['code'] : '';
     $verbose = isset($_GET['verbose']) ? "verbose=true" : "verbose=false";
     
-	$output = array();
-    // echo "/usr/local/bin/node parse_and_run.js $grammar $code verbose=false";
-    exec("/usr/local/bin/node parse_and_run.js $grammar $code $verbose", $output);
+    $output = array();
+    $exec = "/usr/local/bin/node parse_and_run.js $grammar $code $verbose";
+    exec($exec, $output);
     echo implode($output, "\n");
 ?>
