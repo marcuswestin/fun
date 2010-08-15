fa0a5931aa246723adf8c96cbab2c4b78164dc2d
Marcus Westin <marcus.westin@gmail.com>
2010-08-15 13:56:34 -0400

    Make the output verbose by default

--- parse_and_run.php
+++ parse_and_run.php
@@ -1,7 +1,7 @@
 <?php
     $grammar = isset($_GET['grammar']) ? "grammar=" . $_GET['grammar'] : '';
     $code = isset($_GET['code']) ? "code=fun_code/" . $_GET['code'] : '';
-    $verbose = isset($_GET['verbose']) ? "verbose=true" : "verbose=false";
+    $verbose = isset($_GET['verbose']) ? "verbose=" . $_GET['verbose'] : '';
     
     $output = array();
     $exec = "/usr/local/bin/node parse_and_run.js $grammar $code $verbose";
