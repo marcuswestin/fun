d26843b4c62406af76702f962268ccdbd913cade
Marcus Westin <marcus.westin@gmail.com>
2010-10-29 22:03:09 -0400

    Load fin/jsio on the fun app pages

--- new_parser/index.php
+++ new_parser/index.php
@@ -1,5 +1,8 @@
 <?php
 $exec = "/usr/local/bin/node run.js";
 exec($exec, $output);
-echo "<!doctype html>\n<html>\n<head></head>\n<body>\n<script>\n" . implode($output, "\n") . "\n</script>\n</body>\n</html>";
+echo "<!doctype html>\n<html>\n<head></head>\n<body>"
+	. "\n<script src='../lib/fin/fin.js'></script>"
+	. "\n<script src='lib.js'></script>"
+	. "<script>\n" . implode($output, "\n") . "\n</script>\n</body>\n</html>";
 ?>
