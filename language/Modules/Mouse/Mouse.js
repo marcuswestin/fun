cc956d895fa4047b1efae1249c0ba342c029088d
Marcus Westin <marcus.westin@gmail.com>
2010-12-17 16:00:42 -0800

    Fix the Mouse module, which I totally busted

--- language/Modules/Mouse/Mouse.js
+++ language/Modules/Mouse/Mouse.js
@@ -1,18 +1,18 @@
 /* Mouse.x and Mouse.y
  *********************/
 fun.on(document, 'mousemove', function(e) {
-	fun.mutate('SET', 'LOCAL', '__mouseX__', [e.clientX])
-	fun.mutate('SET', 'LOCAL', '__mouseY__', [e.clientY])
+	fun.mutate('SET', 'LOCAL', '__Mouse_x__', [e.clientX])
+	fun.mutate('SET', 'LOCAL', '__Mouse_y__', [e.clientY])
 })
-fun.mutate('SET', 'LOCAL', '__mouseX__', [0])
-fun.mutate('SET', 'LOCAL', '__mouseY__', [0])
+fun.mutate('SET', 'LOCAL', '__Mouse_x__', [0])
+fun.mutate('SET', 'LOCAL', '__Mouse_y__', [0])
 
 /* Mouse.isDown
  **************/
 fun.on(document, 'mousedown', function() {
-	fun.mutate('SET', 'LOCAL', '__mouseIsDown__', [true])
+	fun.mutate('SET', 'LOCAL', '__Mouse_isDown__', [true])
 })
 fun.on(document, 'mouseup', function() {
-	fun.mutate('SET', 'LOCAL', '__mouseIsDown__', [false])
+	fun.mutate('SET', 'LOCAL', '__Mouse_isDown__', [false])
 })
-fun.mutate('SET', 'LOCAL', '__mouseIsDown__', [false])
+fun.mutate('SET', 'LOCAL', '__Mouse_isDown__', [false])
