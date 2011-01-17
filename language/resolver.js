6b6a742b8a4d030e188a6cd9ba8e2bc149df37e3
Marcus Westin <marcus.westin@gmail.com>
2011-01-16 22:06:38 -0500

    Remove reference to _collectDynamicASTs - the function was moved from the resolver to the compiler in 8c189cf0cc2f8ed83db1c9dc31501534f1b45e7c

--- language/resolver.js
+++ language/resolver.js
@@ -303,7 +303,6 @@ var resolveRuntimeIterator = function(context, ast) {
  *****************/
 var resolveIfStatement = function(context, ast) {
 	ast.condition = resolve(context, ast.condition)
-	ast.condition.dynamicASTs = _collectDynamicASTs(ast.condition)
 	ast.ifBlock = resolve(createScope(context), ast.ifBlock)
 	if (ast.elseBlock) {
 		ast.elseBlock = resolve(createScope(context), ast.elseBlock)
