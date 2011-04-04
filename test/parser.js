db5c048d63b553318e4d194979e56eff4532a041
Marcus Westin <marcus.westin@gmail.com>
2011-04-04 10:02:33 -0400

    Start adding unit tests for the parser, using nodeunit

---
+++ test/parser.js
@@ -0,0 +1,40 @@
+var testCase = require('nodeunit').testCase,
+	parser = require('../language/parser'),
+	tokenizer = require('../language/tokenizer')
+
+test('TextLiteral', '"hello world"', static("hello world"))
+test('NumberLiteral', '1', static(1))
+test('Declaration', 'let greeting = "hello"', {
+	type:'DECLARATION',
+	name:'greeting',
+	value: static("hello")
+})
+
+/* UTIL */
+
+function test(name, code, expectedAST) {
+	module.exports['test'+name] = function(assert) {
+		var ast = parse(code)
+		assert.deepEqual(ast, expectedAST)
+		assert.done()
+	}
+}
+
+function static(value) {
+	var ast = { type:'STATIC', value:value }
+	ast.valueType = typeof value
+	return ast
+}
+
+function parse(code) {
+	tokens = tokenizer.tokenize(code)
+	return pruneInfo(parser.parse(tokens))
+}
+
+function pruneInfo(ast) {
+	for (var key in ast) {
+		if (key == 'info') { delete ast[key] }
+		else if (typeof ast[key] == 'object') { pruneInfo(ast[key]) }
+	}
+	return ast
+}
