ac4c3655aae67e9bb3d3bfdd3cea953cee399a06
Marcus Westin <marcus.westin@gmail.com>
2011-04-06 00:59:19 -0400

    Clean up the parser's resulting AST - there should be no null ASTs, and no nested arrays

--- test/testCases/parser.js
+++ test/testCases/parser.js
@@ -15,6 +15,7 @@ test('addition', '1+1', a.composite(a.static(1), '+', a.static(1)))
 test('parenthesized subtraction', '(((1-1)))', a.composite(a.static(1), '-', a.static(1)))
 test('simple if statement', 'if (1 < 2) { 1 }', a.ifElse(a.composite(a.static(1), '<', a.static(2)), a.static(1)))
 test('has no null statements or expressions', '\nlet foo="bar"\n1\n\n', [a.declaration("foo",a.static("bar")), a.static(1)])
+test('parses empty program', '', [])
 
 /* UTIL */
 function test(name, code, expectedAST) {
@@ -31,9 +32,14 @@ function parse(code) {
 }
 
 function pruneInfo(ast) {
-	for (var key in ast) {
-		if (key == 'info') { delete ast[key] }
-		else if (typeof ast[key] == 'object') { pruneInfo(ast[key]) }
+	if (std.isArray(ast)) {
+		return std.map(ast, pruneInfo)
+	} else {
+		for (var key in ast) {
+			if (key == 'info') { delete ast[key] }
+			else if (typeof ast[key] == 'object') { pruneInfo(ast[key]) }
+		}
+		return ast
 	}
-	return ast
 }
+
