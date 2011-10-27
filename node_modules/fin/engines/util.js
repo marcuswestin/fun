bd509ea00bb76ca2c3100d5a4fa0bd7af5bccb38
Marcus Westin <marcus.westin@gmail.com>
2011-10-26 18:17:24 -0700

    use npm instead of hard coded node_modules

--- node_modules/fin/engines/util.js
+++
@@ -1,7 +0,0 @@
-exports.create = function(oldObject) {
-	function F() {}
-	F.prototype = oldObject;
-	var newObject = new F();
-	if (newObject.initialize) { newObject.initialize() }
-	return newObject
-}
