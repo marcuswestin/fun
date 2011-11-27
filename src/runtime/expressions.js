f8d2d2a79018ba9543b609888ead840e2e4c52a2
Marcus Westin <marcus.westin@gmail.com>
2011-11-27 14:22:04 -0800

    Stub out the equals operator. Right now only atomic and composite values can be equal to other atomic values. This adds the logic type, with the two values true and false

--- src/runtime/expressions.js
+++ src/runtime/expressions.js
@@ -5,7 +5,7 @@ var proto = require('std/proto'),
 /* Atomic expressions
  ********************/
 var atomicBase = {
-	toString:function() { return this.content.toString() },
+	asRawString:function() { return this.content.toString() },
 	// asText:function() { return text(this.toString()) },
 	atomic:true,
 	evaluate:function(chain, defaultToUndefined) {
@@ -13,7 +13,8 @@ var atomicBase = {
 			return defaultToUndefined ? undefined : nullValue
 		}
 		return this
-	}
+	},
+	equals:function(that) { return logic(this.type == that.type && this.content == that.content) }
 }
 
 var number = module.exports.number = proto(function(content) { this.content = content }, extend(atomicBase, {
@@ -23,21 +24,41 @@ var number = module.exports.number = proto(function(content) { this.content = co
 var text = module.exports.text = proto(function(content) { this.content = content }, extend(atomicBase, {
 	type:'text'
 }))
-module.exports.string = module.exports.text // don't like this...
+
+var logic = module.exports.logic = function(content) {
+	return content ? trueValue : falseValue
+}
+
+module.exports.null = function() {
+	return nullValue
+}
+
+var logicProto = proto(function(content) { this.content = content }, extend(atomicBase, {
+	type:'logic',
+	asRawString:function() { return '<Bool True>'}
+}))
+
+var trueValue = logicProto(true),
+	falseValue = logicProto(false)
 
 var nullValue = {
 	type:'null',
 	atomic:true,
-	toString:function() { return '<NULL>' },
-	evaluate:function() { return this }
+	asRawString:function() { return '<Null>' },
+	evaluate:function() { return this },
+	equals:function(that) { return falseValue }
 }
-module.exports.null = function() { return nullValue }
+
+
+// don't like these...
+module.exports.string = module.exports.text
+module.exports.boolean = module.exports.logic
 
 /* Collection expressions
  ************************/
 var collectionBase = {
 	atomic:false,
-	toString:function() { throw new Error("Implement collections' toString") },
+	asRawString:function() { throw new Error("Implement collections' asRawString") },
 	evaluate:function(chain, defaultToUndefined) {
 		var value = this
 		if (chain) {
@@ -48,7 +69,8 @@ var collectionBase = {
 		}
 		if (!value) { return nullValue }
 		return value
-	}
+	},
+	equals:function(that) { return falseValue }
 }
 
 var dictionary = module.exports.dictionary = proto(function(content) { this.content = content }, extend(collectionBase, {
@@ -61,12 +83,14 @@ var composite = module.exports.composite = proto(function(left, operator, right)
 	type:'composite',
 	evaluate:function(chain, defaultToUndefined) {
 		return operators[this.operator](this.left.evaluate(), this.right.evaluate()).evaluate(chain, defaultToUndefined)
-	}
+	},
+	equals:function(that) { return this.evaluate().equals(that) }
 })
 
 var variable = module.exports.variable = proto(function(content) { if (!content.evaluate) { asdasd }; this.content = content, this.observers = {} }, {
 	type:'variable',
 	evaluate:function(chain, defaultToUndefined) {
 		return this.content.evaluate(chain, defaultToUndefined)
-	}
+	},
+	equals:function(that) { return this.content.equals(that) }
 })
--- src/runtime/operators.js
+++ src/runtime/operators.js
@@ -1,12 +1,15 @@
 var expressions = require('./expressions')
 
 module.exports = {
-	'+': add
+	'+': add,
+	'==': equals // I wonder if we should make this just =...
 }
 
 function add(left, right) {
 	if (left.type == 'number' && right.type == 'number') {
 		return expressions.number(left.content + right.content)
 	}
-	return expressions.text(left.toString() + right.toString())
+	return expressions.text(left.asRawString() + right.asRawString())
 }
+
+function equals(left, right) { return left.evaluate().equals(right.evaluate()) }
