4950bb33d5657a51d3dd61bb61c0dbafca0034b7
Marcus Westin <marcus.westin@gmail.com>
2012-04-25 15:16:32 -0700

    Make it easier to specify which tests to be run

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -1,2 +1,17 @@
 var reporter = require('nodeunit').reporters.default;
-reporter.run(['test/tests']);
+
+var run = function(files) {
+	reporter.run(['test/tests/'+files[0]], null, function(err) {
+		if (err) { return }
+		files.shift()
+		if (files.length) { run(files) }
+	})
+}
+
+var tests = []
+tests.push('test-1-runtime-library.js')
+tests.push('test-2-parser.js')
+tests.push('test-3-resolver.js')
+tests.push('test-4-compiler.js')
+
+run(tests)
